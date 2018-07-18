169c4fba31c9df278b31d9db31bac8c145e5b710
Electrox-Qui-Mortem <Electrox-Qui-Mortem@users.noreply.github.com>
2018-07-18 14:53:30 -0400

    Update nuke.js

--- commands/nuke.js
+++ commands/nuke.js
@@ -2,12 +2,20 @@ const Discord = require("discord.js");
 const auth = require('../auth.json');
 const owner = auth.ownerid;
 module.exports.run = async function(bot, message, args){
-    message.channel.fetchMessages()
+    message.delete().catch(O_o => {});
+    message.channel.fetchMessages({limit:args[0]})
     .then(function(list){
         message.channel.bulkDelete(list);
+        
     }, function(err){
-        message.channel.send("ERROR: ERROR CLEARING CHANNEL.");
+        
     })
+    .then(function(){
+        message.send(`Successfully deleted ${parseFloat(args)} messages`);
+    },function(err){
+        message.channel.send("ERROR: ERROR CLEARING CHANNEL.");
+        console.log(err)
+    });
 }
 
 module.exports.help = {
